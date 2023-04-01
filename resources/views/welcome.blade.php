d0e092ea9456c0c399695c78342671329f96a04c
FAHIM HASAN <31102498+fahimhasan2020@users.noreply.github.com>
2023-04-01 22:22:07 +0600

    test update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -72,7 +72,7 @@
                 </div>
             <div class="content">
                 <div class="title m-b-md">
-                  Onek Kothin Sheba API
+                  Beshi Kothin Sheba API
                 </div>
 
                 <div class="links">
